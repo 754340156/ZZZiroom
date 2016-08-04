346f63e77d4441671b8576f434b292a435e6587d
lanou3g <754340156@qq.com>
2016-08-04 17:05:27 +0800

    初始化工程

---
+++ ZZZiroom/ZZZiroom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZZiroom
+//
+//  Created by lanou3g on 16/8/4.
+//  Copyright © 2016年 lanou3g-22赵哲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
